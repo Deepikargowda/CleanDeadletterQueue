39924d2fc0d67081e93ed39ed1f79eecc5c48672
Deepikargowda <73956525+Deepikargowda@users.noreply.github.com>
2020-11-05 13:54:31 -0500

    Update Function3.cs

--- Function3.cs
+++ Function3.cs
@@ -1,19 +1,19 @@
-//using System;
-//using Microsoft.Azure.WebJobs;
-//using Microsoft.Azure.WebJobs.Host;
-//using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 
-//namespace FunctionApp6
-//{
-    //public static class Function3
-    //{
+namespace FunctionApp6
+{
+    public static class Function3
+    {
         //This is a test function to force messages to the DLQ
-       // [FunctionName("Function3")]
-        //public static void Run([ServiceBusTrigger("%myqueuereal%", Connection = "ServiceBusConnectionString",IsSessionsEnabled = true)] string myQueueItem, ILogger log)
-        //{
+        [FunctionName("Function3")]
+        public static void Run([ServiceBusTrigger("%myqueuereal%", Connection = "ServiceBusConnectionString",IsSessionsEnabled = true)] string myQueueItem, ILogger log)
+        {
 
-          //  log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
-          //  throw new Exception();
-      //  }
-   // }
-//}
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            throw new Exception();
+        }
+    }
+}
